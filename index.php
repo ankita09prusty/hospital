7247c4a5468cce3007e863f864a6172aa90e966e
Amritsai <amritsaimoharana@gmail.com>
2023-05-19 17:01:45 +0530

    Sub Headings changed and Paragraph Added

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
-
 <?php
 include_once('hms/include/config.php');
-if(isset($_POST['submit']))
-{
-$name=$_POST['fullname'];
-$email=$_POST['emailid'];
-$mobileno=$_POST['mobileno'];
-$dscrption=$_POST['description'];
-$query=mysqli_query($con,"insert into tblcontactus(fullname,email,contactno,message) value('$name','$email','$mobileno','$dscrption')");
-echo "<script>alert('Your information succesfully submitted');</script>";
-echo "<script>window.location.href ='index.php'</script>";
-
+if (isset($_POST['submit'])) {
+    $name = $_POST['fullname'];
+    $email = $_POST['emailid'];
+    $mobileno = $_POST['mobileno'];
+    $dscrption = $_POST['description'];
+    $query = mysqli_query($con, "insert into tblcontactus(fullname,email,contactno,message) value('$name','$email','$mobileno','$dscrption')");
+    echo "<script>alert('Your information succesfully submitted');</script>";
+    echo "<script>window.location.href ='index.php'</script>";
 } ?>
 <!doctype html>
 <html lang="en">
@@ -23,21 +20,21 @@ echo "<script>window.location.href ='index.php'</script>";
     <link rel="shortcut icon" href="assets/images/fav.jpg">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/fontawsom-all.min.css">
-     <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
 </head>
 
-    <body>
+<body>
 
     <!-- ################# Header Starts Here#######################--->
-    
-      <header id="menu-jk">
-    
+
+    <header id="menu-jk">
+
         <div id="nav-head" class="header-nav">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-2 col-md-3  col-sm-12" style="color:#000;font-weight:bold; font-size:30px; margin-top: 1% !important;">Prime Care
-                       <a data-toggle="collapse" data-target="#menu" href="#menu" ><i class="fas d-block d-md-none small-menu fa-bars"></i></a>
+                        <a data-toggle="collapse" data-target="#menu" href="#menu"><i class="fas d-block d-md-none small-menu fa-bars"></i></a>
                     </div>
                     <div id="menu" class="col-lg-8 col-md-9 d-none d-md-block nav-item">
                         <ul>
@@ -46,7 +43,7 @@ echo "<script>window.location.href ='index.php'</script>";
                             <li><a href="#about_us">About Us</a></li>
                             <li><a href="#gallery">Gallery</a></li>
                             <li><a href="#contact_us">Contact Us</a></li>
-                            <li><a href="#logins">Logins</a></li>  
+                            <li><a href="#logins">Logins</a></li>
                         </ul>
                     </div>
                     <div class="col-sm-2 d-none d-lg-block appoint">
@@ -57,8 +54,8 @@ echo "<script>window.location.href ='index.php'</script>";
             </div>
         </div>
     </header>
-    
-     <!-- ################# Slider Starts Here#######################--->
+
+    <!-- ################# Slider Starts Here#######################--->
 
     <div class="slider-detail">
 
@@ -69,7 +66,7 @@ echo "<script>window.location.href ='index.php'</script>";
             </ol>
 
 
-   
+
 
 
             <div class="carousel-inner">
@@ -77,25 +74,24 @@ echo "<script>window.location.href ='index.php'</script>";
                     <img class="d-block w-100" src="assets/images/slider/slider_2.jpg" alt="Second slide">
                     <div class="carousel-cover"></div>
                     <div class="carousel-caption vdg-cur d-none d-md-block">
-                        <h5 class="animated bounceInDown">Hospital Management System</h5>
-            
-                         
-                    
+                        <h5 class="animated bounceInDown">Optimizing Patient Care : PrimeCare</h5>
+
+
+
                     </div>
                 </div>
-                
+
                 <div class="carousel-item active">
                     <img class="d-block w-100" src="assets/images/slider/slider_3.jpg" alt="Third slide">
-                      <div class="carousel-cover"></div>
+                    <div class="carousel-cover"></div>
                     <div class="carousel-caption vdg-cur d-none d-md-block">
-                        <h5 class="animated bounceInDown">Hospital Management System</h5>
-            
-                         
-                    
+                        <h5 class="animated bounceInDown">Most Experienced Doctors & Staffs </h5>
+                        <h7 class="animated bounceInDown">Welcome to our hospital, where expertise meets compassion. Our team of experienced doctors and staff is dedicated to providing exceptional healthcare services to our patients. With years of training and hands-on experience, our doctors bring a wealth of knowledge and expertise to the table. </h7>
+
                     </div>
-              
+
                 </div>
-                
+
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -109,13 +105,13 @@ echo "<script>window.location.href ='index.php'</script>";
 
 
     </div>
-    
-  <!--  ************************* Logins ************************** -->
-    
-    
-     <section id="logins" class="our-blog container-fluid">
+
+    <!--  ************************* Logins ************************** -->
+
+
+    <section id="logins" class="our-blog container-fluid">
         <div class="container">
-        <div class="inner-title">
+            <div class="inner-title">
 
                 <h2>Logins</h2>
             </div>
@@ -124,7 +120,7 @@ echo "<script>window.location.href ='index.php'</script>";
                     <div class="col-sm-4 blog-smk">
                         <div class="blog-single">
 
-                                <img src="assets/images/patient.jpg" alt="">
+                            <img src="assets/images/patient.jpg" alt="">
 
                             <div class="blog-single-det">
                                 <h6>Patient Login</h6>
@@ -137,7 +133,7 @@ echo "<script>window.location.href ='index.php'</script>";
                     <div class="col-sm-4 blog-smk">
                         <div class="blog-single">
 
-                                <img src="assets/images/doctor.jpg" alt="">
+                            <img src="assets/images/doctor.jpg" alt="">
 
                             <div class="blog-single-det">
                                 <h6>Doctors login</h6>
@@ -147,32 +143,32 @@ echo "<script>window.location.href ='index.php'</script>";
                             </div>
                         </div>
                     </div>
-                    
+
                     <div class="col-sm-4 blog-smk">
                         <div class="blog-single">
 
-                                <img src="assets/images/admin.jpg" alt="">
+                            <img src="assets/images/admin.jpg" alt="">
 
                             <div class="blog-single-det">
                                 <h6>Admin Login</h6>
-                    
+
                                 <a href="hms/admin" target="_blank">
                                     <button class="btn btn-success btn-sm">Click Here</button>
                                 </a>
                             </div>
                         </div>
                     </div>
-                    
-                    
-                    
 
-                    
-                    
+
+
+
+
+
                 </div>
             </div>
-            
+
         </div>
-    </section>  
+    </section>
 
 
 
@@ -208,7 +204,7 @@ echo "<script>window.location.href ='index.php'</script>";
 
                 <div class="col-lg-4 col-md-6">
                     <div class="single-key">
-                       <i class="fab fa-monero"></i>
+                        <i class="fab fa-monero"></i>
                         <h5>Neurologist</h5>
                     </div>
                 </div>
@@ -246,135 +242,135 @@ echo "<script>window.location.href ='index.php'</script>";
         </div>
 
     </section>
-    
-    
-  
-    
+
+
+
+
     <!--  ************************* About Us Starts Here ************************** -->
-        
+
     <section id="about_us" class="about-us">
         <div class="row no-margin">
             <div class="col-sm-6 image-bg no-padding">
-                
+
             </div>
             <div class="col-sm-6 abut-yoiu">
                 <h3>About Our Hospital</h3>
-<?php
-$ret=mysqli_query($con,"select * from tblpage where PageType='aboutus' ");
-while ($row=mysqli_fetch_array($ret)) {
-?>
+                <?php
+                $ret = mysqli_query($con, "select * from tblpage where PageType='aboutus' ");
+                while ($row = mysqli_fetch_array($ret)) {
+                ?>
 
-    <p><?php  echo $row['PageDescription'];?>.</p><?php } ?>
+                    <p><?php echo $row['PageDescription']; ?>.</p><?php } ?>
             </div>
         </div>
-    </section>    
-    
-    
-            <!--  ************************* Gallery Starts Here ************************** -->
-        <div id="gallery" class="gallery">    
-           <div class="container">
-              <div class="inner-title">
+    </section>
+
+
+    <!--  ************************* Gallery Starts Here ************************** -->
+    <div id="gallery" class="gallery">
+        <div class="container">
+            <div class="inner-title">
 
                 <h2>Our Gallery</h2>
                 <p>View Our Gallery</p>
             </div>
-              <div class="row">
-                
+            <div class="row">
 
-        <div class="gallery-filter d-none d-sm-block">
-            <button class="btn btn-default filter-button" data-filter="all">All</button>
-            <button class="btn btn-default filter-button" data-filter="hdpe">Dental</button>
-            <button class="btn btn-default filter-button" data-filter="sprinkle">Cardiology</button>
-            <button class="btn btn-default filter-button" data-filter="spray"> Neurology</button>
-            <button class="btn btn-default filter-button" data-filter="irrigation">Laboratry</button>
+
+                <div class="gallery-filter d-none d-sm-block">
+                    <button class="btn btn-default filter-button" data-filter="all">All</button>
+                    <button class="btn btn-default filter-button" data-filter="hdpe">Dental</button>
+                    <button class="btn btn-default filter-button" data-filter="sprinkle">Cardiology</button>
+                    <button class="btn btn-default filter-button" data-filter="spray"> Neurology</button>
+                    <button class="btn btn-default filter-button" data-filter="irrigation">Laboratry</button>
+                </div>
+                <br />
+
+
+
+                <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                    <img src="assets/images/gallery/gallery_01.jpg" class="img-responsive">
+                </div>
+
+                <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter sprinkle">
+                    <img src="assets/images/gallery/gallery_02.jpg" class="img-responsive">
+                </div>
+
+                <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                    <img src="assets/images/gallery/gallery_03.jpg" class="img-responsive">
+                </div>
+
+                <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter irrigation">
+                    <img src="assets/images/gallery/gallery_04.jpg" class="img-responsive">
+                </div>
+
+                <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter spray">
+                    <img src="assets/images/gallery/gallery_05.jpg" class="img-responsive">
+                </div>
+
+
+
+                <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter spray">
+                    <img src="assets/images/gallery/gallery_06.jpg" class="img-responsive">
+                </div>
+
+            </div>
         </div>
-        <br/>
 
 
-
-            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
-                <img src="assets/images/gallery/gallery_01.jpg" class="img-responsive">
-            </div>
-
-            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter sprinkle">
-                <img src="assets/images/gallery/gallery_02.jpg" class="img-responsive">
-            </div>
-
-            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
-                <img src="assets/images/gallery/gallery_03.jpg" class="img-responsive">
-            </div>
-
-            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter irrigation">
-                <img src="assets/images/gallery/gallery_04.jpg" class="img-responsive">
-            </div>
-
-            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter spray">
-                <img src="assets/images/gallery/gallery_05.jpg" class="img-responsive">
-            </div>
-
-          
-
-            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter spray">
-                <img src="assets/images/gallery/gallery_06.jpg" class="img-responsive">
-            </div>
-
-        </div>
     </div>
-       
-       
-       </div>
-        <!-- ######## Gallery End ####### -->
-    
-    
-     <!--  ************************* Contact Us Starts Here ************************** -->
-    
+    <!-- ######## Gallery End ####### -->
+
+
+    <!--  ************************* Contact Us Starts Here ************************** -->
+
     <section id="contact_us" class="contact-us-single">
         <div class="row no-margin">
 
-            <div  class="col-sm-12 cop-ck">
+            <div class="col-sm-12 cop-ck">
                 <form method="post">
-                <h2 >Contact Form</h2>
+                    <h2>Contact Form</h2>
                     <div class="row cf-ro">
-                        <div  class="col-sm-3"><label>Enter Name :</label></div>
-                        <div class="col-sm-8"><input type="text" placeholder="Enter Name" name="fullname" class="form-control input-sm" required ></div>
+                        <div class="col-sm-3"><label>Enter Name :</label></div>
+                        <div class="col-sm-8"><input type="text" placeholder="Enter Name" name="fullname" class="form-control input-sm" required></div>
                     </div>
-                    <div  class="row cf-ro">
-                        <div  class="col-sm-3"><label>Email Address :</label></div>
-                        <div class="col-sm-8"><input type="text" name="emailid" placeholder="Enter Email Address" class="form-control input-sm"  required></div>
+                    <div class="row cf-ro">
+                        <div class="col-sm-3"><label>Email Address :</label></div>
+                        <div class="col-sm-8"><input type="text" name="emailid" placeholder="Enter Email Address" class="form-control input-sm" required></div>
                     </div>
-                     <div  class="row cf-ro">
-                        <div  class="col-sm-3"><label>Mobile Number:</label></div>
-                        <div class="col-sm-8"><input type="text" name="mobileno" placeholder="Enter Mobile Number" class="form-control input-sm" required ></div>
+                    <div class="row cf-ro">
+                        <div class="col-sm-3"><label>Mobile Number:</label></div>
+                        <div class="col-sm-8"><input type="text" name="mobileno" placeholder="Enter Mobile Number" class="form-control input-sm" required></div>
                     </div>
-                     <div  class="row cf-ro">
-                        <div  class="col-sm-3"><label>Enter  Message:</label></div>
+                    <div class="row cf-ro">
+                        <div class="col-sm-3"><label>Enter Message:</label></div>
                         <div class="col-sm-8">
-                          <textarea rows="5" placeholder="Enter Your Message" class="form-control input-sm" name="description" required></textarea>
+                            <textarea rows="5" placeholder="Enter Your Message" class="form-control input-sm" name="description" required></textarea>
                         </div>
                     </div>
-                     <div  class="row cf-ro">
-                        <div  class="col-sm-3"><label></label></div>
+                    <div class="row cf-ro">
+                        <div class="col-sm-3"><label></label></div>
                         <div class="col-sm-8">
-                         <button class="btn btn-success btn-sm" type="submit" name="submit">Send Message</button>
+                            <button class="btn btn-success btn-sm" type="submit" name="submit">Send Message</button>
                         </div>
-                </div>
-            </form>
+                    </div>
+                </form>
             </div>
-     
+
         </div>
     </section>
-    
-    
-    
-    
-    
+
+
+
+
+
     <!-- ################# Footer Starts Here#######################--->
 
 
     <footer class="footer">
         <div class="container">
             <div class="row">
-       
+
                 <div class="col-md-6 col-sm-12">
                     <h2>Useful Links</h2>
                     <ul class="list-unstyled link-list">
@@ -389,19 +385,19 @@ while ($row=mysqli_fetch_array($ret)) {
                     <h2>Contact Us</h2>
                     <address class="md-margin-bottom-40">
 
-<?php
-$ret=mysqli_query($con,"select * from tblpage where PageType='contactus' ");
-while ($row=mysqli_fetch_array($ret)) {
-?>
+                        <?php
+                        $ret = mysqli_query($con, "select * from tblpage where PageType='contactus' ");
+                        while ($row = mysqli_fetch_array($ret)) {
+                        ?>
 
 
-                        <?php  echo $row['PageDescription'];?> <br>
-                        Phone: <?php  echo $row['MobileNumber'];?> <br>
-                        Email: <a href="mailto:<?php  echo $row['Email'];?>" class=""><?php  echo $row['Email'];?></a><br>
-                        Timing: <?php  echo $row['OpenningTime'];?>
+                            <?php echo $row['PageDescription']; ?> <br>
+                            Phone: <?php echo $row['MobileNumber']; ?> <br>
+                            Email: <a href="mailto:<?php echo $row['Email']; ?>" class=""><?php echo $row['Email']; ?></a><br>
+                            Timing: <?php echo $row['OpenningTime']; ?>
                     </address>
 
-        <?php } ?>
+                <?php } ?>
 
 
 
@@ -410,19 +406,19 @@ while ($row=mysqli_fetch_array($ret)) {
                 </div>
             </div>
         </div>
-        
+
 
     </footer>
     <div class="copy">
-            <div class="container">
-         Hospital Management System
-                
-     
-            </div>
+        <div class="container">
+            Hospital Management System
+
 
         </div>
-    
-    </body>
+
+    </div>
+
+</body>
 
 <script src="assets/js/jquery-3.2.1.min.js"></script>
 <script src="assets/js/popper.min.js"></script>
